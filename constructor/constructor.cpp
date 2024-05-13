d2d3fa682cea195e7f96a07fb71de1feeba1a468
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2024-05-13 11:38:41 +0700

    constructor untuk mahasiswa

---
+++ constructor/constructor.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+public:
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim)
+    {
+        nim = iNim;
+    }
+    Mahasiswa(string iNama)
+    {
+        nama = iNama;
+    }
+    Mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    }
+    void cetak()
+    {
+        cout << endl << "Nim =" << nim << endl;
+        cout << "Nama = " << nama << endl;
+    }
+};
+
+int main()
+{
+
+}
